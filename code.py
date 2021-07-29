f9f2bb1a5f808a156de19550ee2fb4a106a3e577
Kulbhushan Karn <67707132+Kulbhushankarn@users.noreply.github.com>
2021-07-29 12:07:51 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n")
+print ("Please Enter The Temperature Below In Degree Celsius")
+print ("Enter the temperature value here:\n-->>>")
+celsius = int(input())
+Fahrenheit= celsius * 9/5 + 32
+print ("Converting To Degree Fahrenheit ")
+import time
+time.sleep (2)
+print(int(Fahrenheit),("Degree Fahrenheit"))
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n")
